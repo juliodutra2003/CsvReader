eed098948c52cebec3d33a26cc650bf1eea95bc5
jcbadboy <juliodutra2003@yahoo.com.br>
2017-01-12 15:52:07 -0200

    input sanitization improved

--- cvsreader/src/net/crazyminds/controller/InputController.java
+++ cvsreader/src/net/crazyminds/controller/InputController.java
@@ -3,6 +3,8 @@ package net.crazyminds.controller;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.Consumer;
 
 import net.crazyminds.Command.Count;
 import net.crazyminds.Command.Filter;
@@ -106,17 +108,17 @@ public class InputController {
 
 	private static void ProcessCommand(String input) {
 		Response response = new Response();
-		String[] commandline = input.split(" ");
-		commandline = Sanitize(commandline);
-		String command = commandline[0];
+		String[] commandline = Sanitize(input);
 		
-		if(command == null)
+		if(commandline == null)
 		{	
 			response.setStatus(true);
 			response.setMessage("");
 			View.ShowCommandOkMessage( response );
 			return;
 		}
+		
+		String command = commandline[0];
 		
 		switch(command)
 		{
@@ -165,19 +167,71 @@ public class InputController {
 	 *  
 	 * @param commandline is the name of the file to read.
 	 * 
-	 * @return return sanitized string[] line command 
+	 * @return return sanitized string[] line command . Null when no valid input. 
 	 */
-	static String[] Sanitize(String[] commandline){
-		ArrayList<String> sanitizedCommandline = new ArrayList<String>();
-		 
-		for(String s: commandline)
+	static String[] Sanitize(String input){
+		
+		//return invalid command
+		if(input.equals(null)||input.equals(""))
+			return null;
+		
+		String[] inputSplited = input.split("\\s");
+		
+		if(inputSplited.length > 0)//add command
 		{
-			if( !s.equals("") && !s.equals(" ") )
+			ArrayList<String> commandline = new ArrayList<String>();
+			
+			commandline.add(inputSplited[0]); //add first parameter
+			if(inputSplited.length > 1)
 			{
-				sanitizedCommandline.add(s);
-			}
+				commandline.add(inputSplited[1]);	
+				
+				if(inputSplited.length > 2) //add rest of comandline
+				{
+					for (int i = 2 ; i < inputSplited.length; i++ )
+						commandline.add(inputSplited[i]);
+				}	
+			}	
+			//return valid command
+			return commandline.toArray(new String[commandline.size()]);
+		}
+		else //return invalid command
+		{
+			return null;
 		}
 		
-		return sanitizedCommandline.toArray(commandline);		
+//		ArrayList<String> sanitizedCommandline = new ArrayList<String>();
+//		 
+//		System.out.print("sanitizedCommandline: raw commandline ");
+//		for (String c: commandline)
+//		{
+//			System.out.print(c+",");
+//		}
+//		System.out.println("");
+//		
+//		for(String s: commandline)
+//		{
+//			if( s.contains("'") )
+//			{
+//				int firstIndex = s.indexOf("'");
+//				int lastIndex = s.lastIndexOf("'");
+//				if(firstIndex < lastIndex)
+//				{
+//					sanitizedCommandline.add(s.substring(firstIndex+1, lastIndex));
+//				}
+//				else //command invalid
+//				{
+//					sanitizedCommandline.add(s);
+//				}
+//			}
+//			else if( !s.equals("") && !s.equals(" ") )
+//			{
+//				sanitizedCommandline.add(s);
+//			}
+//		}
+//		
+//		System.out.println("sanitizedCommandline: " + sanitizedCommandline);
+//		
+//		return sanitizedCommandline.toArray(commandline);		
 	}
 }
