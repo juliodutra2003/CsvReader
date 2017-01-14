8a26dec1e4232af96c658e4c93c0430eea81750a
jcbadboy <juliodutra2003@yahoo.com.br>
2017-01-14 11:28:06 -0200

    class comments

--- cvsreader/src/net/crazyminds/Command/Command.java
+++ cvsreader/src/net/crazyminds/Command/Command.java
@@ -2,6 +2,15 @@ package net.crazyminds.Command;
 
 import net.crazyminds.controller.Response;
 
+/**
+ * 
+ * @author julio
+ *
+ * Command interfaces - Signature for every command sub-class
+ * 
+ * (Strategy Pattern)
+ */
+
 public interface Command {
 
 	public Response<?> Interpret(String[] commandline);
